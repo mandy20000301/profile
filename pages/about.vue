ca9a4aca4ce34225986a159017579d7573182519
Mandy_Li <mandy@digisalad.cool>
2023-08-19 23:45:16 +0800

    init

---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1 class="text-[30px] text-blue-800">about</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-[20px] text-violet-600 font-bold text-center">Hello</h1>
+        <img src="https://picsum.photos/300/200/?random=10">
+        <nuxt-link to="/about">about</nuxt-link>
+    </div>
+</template>

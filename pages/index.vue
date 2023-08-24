364019bb625e7a0e915c4f8a8ad40475b88605e0
Mandy_Li <mandy@digisalad.cool>
2023-08-24 14:06:20 +0800

    add image

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h1 class="text-[20px] text-gray-800 font-bold text-center">Hello</h1>
-        <nuxt-link to="/about" class="underline">about</nuxt-link>
+        <h1>Mandy</h1>
     </div>
 </template>

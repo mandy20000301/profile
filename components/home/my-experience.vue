02b8934ae88ecc843b08203ccb21028d25520d68
Mandy_Li <mandy@digisalad.cool>
2023-08-26 23:35:18 +0800

    experience tab section

---
+++ components/home/my-experience.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <SharedTabBox :info="tabList" @update-activeId="updateActiveId($event)" />
+
+    </div>
+</template>
+<script setup>
+const tabList = ref(
+    [
+        {
+            id: 'experience',
+            title: '過往經歷'
+        },
+        {
+            id: 'skill',
+            title: '專業技能'
+        }
+    ]);
+const activeId = ref(null);
+const updateActiveId = (val => activeId.value = val)
+
+
+</script>     

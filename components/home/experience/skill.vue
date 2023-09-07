95161ca99ac2d6e8ddc8fd8ec582f4885e425398
Mandy_Li <mandy@digisalad.cool>
2023-09-07 13:58:24 +0800

    skill logo -update

--- components/home/experience/skill.vue
+++ components/home/experience/skill.vue
@@ -1,25 +1,12 @@
 <template>
-    <div class="container pb-[60px] md:pb-[120px]" v-fade:reveal>
-        <div class="grid grid-cols-4 gap-8 md:grid-cols-5 md:gap-x-10 md:gap-y-10 w-[80%] lg:w-[60%] mx-auto hidden-content">
-            <div class=" flex items-center justify-center" v-for="(i,idx) in skillsArr" :key="`${i}-${idx}`" v-fade:in >
-                <img :src="`/image/skills/${i}.png`" :alt="i" class="  " >
+    <div class="container pb-[60px] md:pb-[120px]">
+        <div class="grid grid-cols-4 gap-4 md:grid-cols-6 md:gap-10 w-[100%] md:w-[80%] mx-auto hidden-content">
+            <div class="flex items-center justify-center" v-for="(i,idx) in skillsArr" :key="`${i}-${idx}`" v-fade:in >
+                <img :src="`/image/skills/${i}.png`" :alt="i" :title="i" >
             </div>
         </div>
     </div>
 </template>
 <script setup>
-    const logos = ref([
-        {
-            id:'html',
-            title:'html',
-            image:'html'
-        },
-        {
-            id:'css',
-            title:'css',
-            image:'css'
-        },
-    ]);
-    const skillsArr = ref(['html','css1','javascript','rwd','vue','nuxt','php','mysql','pinia','jquery','react','figma','git','github','sourcetree','postman','bitbucket','tailwindcss','sass','bootstrap']);
-    const arr = ref([...logos.value,...logos.value,...logos.value,...logos.value])
+    const skillsArr = ref(['html','css','javascript','rwd','vue','nuxt','react','pinia','cicd','tailwindcss','sass','bootstrap','php','mysql','jquery','git','github','sourcetree','bitbucket','gulp','webpack','postman','python','figma']);
 </script>

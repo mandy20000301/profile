1c2b3ed6abdeaf620e7121f95c31abcd095acc3a
Mandy_Li <mandy@digisalad.cool>
2023-12-30 14:19:54 +0800

    Revert "del unusage code"
    This reverts commit b8185091bf59348c824d4958e50f1941a252d723.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,11 @@ export default defineNuxtConfig({
                 },
             ],
             link: [
+                // {
+                //     rel: "icon",
+                //     type: "image/x-icon",
+                //     href: "/favicon.ico",
+                // },
                 {
                     rel: "icon",
                     type: "image/png",
